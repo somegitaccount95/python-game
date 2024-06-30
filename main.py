b6a26b2bed67cd2ff080ec3d7508a231aa09f8e6
somgitacount95 <144242256+somegitaccount95@users.noreply.github.com>
2024-06-29 19:07:45 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame
+
+# --init--
+pygame.init()
+screen_size = (700, 500)
+
+screen = pygame.display.set_mode(screen_size)
+pygame.display.set_caption("pygame Test")
+ 
+
+clock = pygame.time.Clock()
+ 
+ 
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+     
+    
+    screen.fill((0,0,0))
+     
+    # Game Loop
+  
+
+    # update
+    pygame.display.flip()
+    clock.tick(60)
+ 
+pygame.quit()
