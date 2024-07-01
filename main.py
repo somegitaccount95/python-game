3fdc7ffed394e2b25bc202923aae9caef77d03a2
Squibst <phd.tokarchuk@icloud.com>
2024-07-01 12:41:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,25 @@ screen = pygame.display.set_mode(screen_size)
 pygame.display.set_caption("pygame Test")
 
 clock = pygame.time.Clock()
- 
+
+right = false
+left = false
  
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-     
+        if event.type = KEYDOWN
+            if event.key = K_RIGHT
+                right = true
+            if event.key = K_LEFT
+                left = true
+        if event.type = KEYUP
+            if event.key = K_RIGHT
+                right = false
+            if event.key = K_LEFT
+                left = false
     
     screen.fill((0,0,0))
      
@@ -27,4 +38,3 @@ while running:
     clock.tick(60)
  
 pygame.quit()
-
