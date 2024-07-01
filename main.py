69a99c4aec9eb19e1a4962b9069094e24ba5a986
Squibst <phd.tokarchuk@icloud.com>
2024-07-01 13:03:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,30 +8,40 @@ screen = pygame.display.set_mode(screen_size)
 pygame.display.set_caption("pygame Test")
 
 clock = pygame.time.Clock()
+from pygame.locals import *
+pygame.init()
 
-right = false
-left = false
- 
+right = False
+left = False
+
+riba = pygame.image.load("riba.png")
+location = [100, 100]
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type = KEYDOWN
-            if event.key = K_RIGHT
-                right = true
-            if event.key = K_LEFT
-                left = true
-        if event.type = KEYUP
-            if event.key = K_RIGHT
-                right = false
-            if event.key = K_LEFT
-                left = false
+        if event.type == KEYDOWN:
+            if event.key == K_RIGHT:
+                right = True
+            if event.key == K_LEFT:
+                left = True
+        if event.type == KEYUP:
+            if event.key == K_RIGHT:
+                right = False
+            if event.key == K_LEFT:
+                left = False
     
-    screen.fill((0,0,0))
+    screen.fill((100,112,230))
      
     # Game Loop
-  
+
+    screen.blit(riba, location)
+    if right:
+        location[0] += 5
+    if left:
+        location[0] -= 5
 
     # Update
     pygame.display.flip()
