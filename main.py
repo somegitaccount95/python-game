f631be3970105802eeab783e3caa782e1024ec7c
Max Repka <maxrepkar@gmail.com>
2024-07-01 13:58:52 -0500

    commit

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ from pygame.locals import *
 pygame.init()
 
 
-keyDict = [K_w, K_a, K_s, K_d, K_UP, K_DOWN, K_LEFT, K_RIGHT]
-keys = {K_w : False, K_a : False, K_s : False, K_d : False, K_UP : False, K_DOWN : False, K_LEFT : False, K_RIGHT : False}
+keyDict = [K_w, K_a, K_s, K_d, K_UP, K_DOWN, K_LEFT, K_RIGHT, K_SPACE]
+keys = {K_w : False, K_a : False, K_s : False, K_d : False, K_UP : False, K_DOWN : False, K_LEFT : False, K_RIGHT : False, K_SPACE : False}
 
 
 riba = player(screen, "Images/Riba.png", 100, 100)
