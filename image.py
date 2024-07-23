0f31805ca0b2d9671f5864dab0c53bfdf9243159
Max Repka <maxrepkar@gmail.com>
2024-07-23 11:54:50 -0500

    delete useless class

--- image.py
+++
@@ -1,9 +0,0 @@
-import pygame
-class image:
-    def __init__(self, image, x, y):
-        self.image = image
-        self.x = x
-        self.y = y
-    
-    def render(self, screen):
-        screen.blit(self.image, (self.x, self.y))
